727a7799fcabaad5b39be78fb4c299af943d8d51
DmitriAleks <Dima.aleks.94@mail.ru>
2022-05-08 13:28:06 +0300

    'rxJx'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
- import {  Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {Observable, interval, Subscription , Subject} from 'rxjs';
+import {filter, map, switchMap} from "rxjs/operators";
+
+
+
 
 export interface Post {
   title: string
-  text : string
+  text: string
   id: any
 }
-
 
 
 @Component({
@@ -15,71 +18,120 @@ export interface Post {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
- e: number = Math.E
+  sub: Subscription
+  stream2$: Subject<number> = new Subject<number>()
+  counter = 0
+  constructor() {
+  this.sub = this.stream2$.subscribe(value=>{
+    console.log('subscribe', value)
+  })
+
+
+
+    // const stream$ = new Observable(observer => {
+    //   setTimeout(() => {
+    //     observer.next(1)
+    //   }, 500)
+    //   setTimeout(() => {
+    //     observer.complete()
+    //   }, 2700)
+    //   setTimeout(() => {
+    //     observer.error('Something error')
+    //   }, 2500)
+    //
+    // })
+    // this.sub = stream$.
+    // subscribe(
+    //   value => console.log('Next', value),
+    //   error => console.log('Error', error),
+    //   ()=> console.log('Complete' )
+    // )
+
+    // const intrervalStream$ = interval(1000)
+    // this.sub = intrervalStream$
+    //   .pipe(
+    //     filter(value => value % 2 === 0),
+    //     map((value)=> `Mapped value ${value}`),
+    //     switchMap(()=> interval(500))
+    //   )
+    //   .subscribe((value) => {
+    //   console.log('value', value)
+    // })
+  }
+
+  e: number = Math.E
   str: string = 'hello wordl'
   isVisible = true
   float = 0.42
-  date: Date = new Date() 
+  date: Date = new Date()
   p: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(()=>{
-resolve('Promise Resolved')
-    },4000)
+    setTimeout(() => {
+      resolve('Promise Resolved')
+    }, 4000)
   })
 
 
-  mDate$: Observable<Date> = new Observable(obs=> {
-    setInterval(()=>{
-obs.next(new Date())
-    },1000)
+  mDate$: Observable<Date> = new Observable(obs => {
+//     setInterval(()=>{
+// obs.next(new Date())
+//     },1000)
   })
 
-  mDate:Date
+  mDate: Date
 
-  search=''
-  searchField:string = 'title'
+  search = ''
+  searchField: string = 'title'
 
-  obj={
-    a:1,b:{
-      c:2,
-      d:{
-        e:3,
-        f:4
+  obj = {
+    a: 1, b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4
       }
     }
   }
 
-posts: Post[] = [{
-  title: "Best Post",
-  text: 'the best text the best',
-  id: 1
-},{
-  title: "no best Post",
-  text: 'the no best text the no best',
-  id: 2
-},{
-  title: "no best Post",
-  text: 'the no best text the no best',
-  id: 3
-}]
+  posts: Post[] = [{
+    title: "Best Post",
+    text: 'the best text the best',
+    id: 1
+  }, {
+    title: "no best Post",
+    text: 'the no best text the no best',
+    id: 2
+  }, {
+    title: "no best Post",
+    text: 'the no best text the no best',
+    id: 3
+  }]
 
-ngOnInit(): void {
-  setTimeout(()=>{
-    const a =  [...this.posts]
-  },3000)
-  this.mDate$.subscribe(date=>{
-this.mDate = date
-  })
-}
+  ngOnInit(): void {
+    // setTimeout(()=>{
+    //   const a =  [...this.posts]
+    // },3000)
+    this.mDate$.subscribe(date => {
+      this.mDate = date
+    })
+  }
 
-updatePosts(post:Post) {
-  this.posts.unshift(post)
+  updatePosts(post: Post) {
+    this.posts.unshift(post)
 
-}
-deletePost(id:any){
-   this.posts = this.posts.filter((post)=>post.id !== id)
-}
+  }
 
+  deletePost(id: any) {
+    this.posts = this.posts.filter((post) => post.id !== id)
+  }
 
+  stop() {
+    this.sub.unsubscribe()
+  }
+
+  next(){
+    this.counter++
+    this.stream2$.next(this.counter)
+  }
 
 
 }
